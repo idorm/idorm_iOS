06ad9c00602dbb51abc3185e1acecf83558ce9ac
eung7 <wannabeeung@gmail.com>
2022-12-13 21:10:15 +0900

    [Feat] ErrorModel 정의

--- idorm/Source/Presentation/Login/ViewModels/LoginVM.swift
+++ idorm/Source/Presentation/Login/ViewModels/LoginVM.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 import RxSwift
 import RxCocoa
 import RxMoya
@@ -50,23 +52,30 @@ final class LoginViewModel: ViewModel {
       .do(onNext: { [weak self] _ in self?.output.isLoading.onNext(true) })
       .flatMap { APIService.memberProvider.rx.request(.login(id: $0.0, pw: $0.1)) }
       .do(onNext: { [weak self] _ in self?.output.isLoading.onNext(false) })
-      .subscribe(onNext: { [weak self] response in
-        switch response.statusCode {
-        case 200: // 로그인 성공
-          let data = response.data
-          let responseModel = APIService.decode(ResponseModel<MemberModel.MyInformation>.self, data: data).data
-          TokenStorage.instance.saveToken(token: responseModel.loginToken ?? "")
-          MemberInfoStorage.instance.saveMyInformation(from: responseModel)
-          SharedAPI.instance.retrieveMyOnboarding()
-          self?.output.presentTabBarVC.onNext(Void())
-        case 400:
-          self?.output.presentPopupVC.onNext("이메일 입력은 필수입니다.")
-        case 404:
-          self?.output.presentPopupVC.onNext("등록 혹은 가입되지 않은 이메일입니다.")
-        case 409:
-          self?.output.presentPopupVC.onNext("올바르지 않은 비밀번호입니다.")
-        default: break
-        }
+      .subscribe(onNext: { response in
+          print(response.description)
+  //        do {
+//          let response = try response.filterSuccessfulStatusCodes()
+//        } catch let error as  {
+//          print(error.)
+//        }
+        
+//        switch response.statusCode {
+//        case 200: // 로그인 성공
+//          let data = response.data
+//          let responseModel = APIService.decode(ResponseModel<MemberModel.MyInformation>.self, data: data).data
+//          TokenStorage.instance.saveToken(token: responseModel.loginToken ?? "")
+//          MemberInfoStorage.instance.saveMyInformation(from: responseModel)
+//          SharedAPI.instance.retrieveMyOnboarding()
+//          self?.output.presentTabBarVC.onNext(Void())
+//        case 400:
+//          self?.output.presentPopupVC.onNext("이메일 입력은 필수입니다.")
+//        case 404:
+//          self?.output.presentPopupVC.onNext("등록 혹은 가입되지 않은 이메일입니다.")
+//        case 409:
+//          self?.output.presentPopupVC.onNext("올바르지 않은 비밀번호입니다.")
+//        default: break
+//        }
       })
       .disposed(by: disposeBag)
   }
