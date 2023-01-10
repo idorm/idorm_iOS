c69049879ff13e5de6498058a1fa5f9fa8ce7d1d
eung7 <wannabeeung@gmail.com>
2023-01-10 10:55:54 +0900

    [Chore] 파일 디렉토리 변경

---
+++ idorm/Source/Presentation/Community/ViewControllers/PostVC.swift
@@ -0,0 +1,31 @@
+//
+//  PostVC.swift
+//  idorm
+//
+//  Created by 김응철 on 2023/01/10.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class PostViewController: BaseViewController {
+  
+  // MARK: - Properties
+  
+  // MARK: - LifeCycle
+  
+  // MARK: - Setup
+  
+  // MARK: - Bind
+}
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+struct PostVC_PreView: PreviewProvider {
+  static var previews: some View {
+    PostViewController().toPreview()
+  }
+}
+#endif
