be24e23e57838671d42bde0141dab4190faeaf8d
eung7 <wannabeeung@gmail.com>
2023-01-13 22:34:58 +0900

    [Feat] PopularPostCell 구현

--- idorm/Source/Presentation/Community/Views/Cells/PopularPostCell.swift
+++ idorm/Source/Presentation/Community/Views/Cells/PopularPostCell.swift
@@ -13,11 +13,67 @@ final class PopularPostCell: UICollectionViewCell, BaseView {
   
   // MARK: - Properties
   
+  private let popularLabel: UILabel = {
+    let lb = UILabel()
+    lb.text = "인기"
+    lb.textColor = .idorm_blue
+    lb.font = .init(name: MyFonts.bold.rawValue, size: 12)
+    
+    return lb
+  }()
+
+  private let contentsLabel: UILabel = {
+    let lb = UILabel()
+    lb.text = "3 기숙사 카페 혜윰메뉴 올려드립니다! 제가"
+    lb.font = .init(name: MyFonts.regular.rawValue, size: 12)
+    lb.textColor = .black
+    lb.lineBreakMode = .byCharWrapping
+    lb.numberOfLines = 0
+    
+    return lb
+  }()
+  
+  private let messageCountLabel: UILabel = {
+    let lb = UILabel()
+    lb.textColor = .idorm_gray_400
+    lb.font = .init(name: MyFonts.regular.rawValue, size: 12)
+    lb.text = "100"
+    
+    return lb
+  }()
+  
+  private let pictureCountLabel: UILabel = {
+    let lb = UILabel()
+    lb.textColor = .idorm_gray_400
+    lb.font = .init(name: MyFonts.regular.rawValue, size: 12)
+    lb.text = "100"
+    
+    return lb
+  }()
+
+  private let likeCountLabel: UILabel = {
+    let lb = UILabel()
+    lb.textColor = .idorm_gray_400
+    lb.font = .init(name: MyFonts.regular.rawValue, size: 12)
+    lb.text = "100"
+    
+    return lb
+  }()
+  
+  private let messageImageView = UIImageView(image: UIImage(named: "speechBubble_double_small"))
+  private let pictureImageView = UIImageView(image: UIImage(named: "picture_small"))
+  private let likeImageView = UIImageView(image: UIImage(named: "thumbsup_small"))
+  private lazy var messageStack = stackView([messageImageView, messageCountLabel])
+  private lazy var pictureStack = stackView([pictureImageView, pictureCountLabel])
+  private lazy var likeStack = stackView([likeImageView, likeCountLabel])
+  
   // MARK: - Initialize
   
   override init(frame: CGRect) {
     super.init(frame: frame)
-    
+    setupStyles()
+    setupLayouts()
+    setupConstraints()
   }
   
   required init?(coder: NSCoder) {
@@ -26,5 +82,58 @@ final class PopularPostCell: UICollectionViewCell, BaseView {
   
   // MARK: - Setup
   
+  func setupStyles() {
+    contentView.layer.cornerRadius = 8
+    contentView.layer.masksToBounds = true
+    contentView.backgroundColor = .white
+  }
   
+  func setupLayouts() {
+    [
+      popularLabel,
+      contentsLabel,
+      messageStack,
+      pictureStack,
+      likeStack
+    ].forEach { contentView.addSubview($0) }
+  }
+  
+  func setupConstraints() {
+    popularLabel.snp.makeConstraints { make in
+      make.leading.equalToSuperview().inset(10)
+      make.top.equalToSuperview().inset(15)
+    }
+    
+    contentsLabel.snp.makeConstraints { make in
+      make.leading.trailing.equalToSuperview().inset(10)
+      make.top.equalTo(popularLabel.snp.bottom).offset(4)
+      make.bottom.equalTo(messageStack.snp.top).offset(-30)
+    }
+    
+    likeStack.snp.makeConstraints { make in
+      make.leading.equalToSuperview().inset(10)
+      make.bottom.equalToSuperview().inset(16.5)
+    }
+    
+    messageStack.snp.makeConstraints { make in
+      make.leading.equalTo(likeStack.snp.trailing).offset(8)
+      make.centerY.equalTo(likeStack)
+    }
+    
+    pictureStack.snp.makeConstraints { make in
+      make.leading.equalTo(messageStack.snp.trailing).offset(8)
+      make.centerY.equalTo(likeStack)
+    }
+  }
+  
+  // MARK: - Helpers
+  
+  private func stackView(_ views: [UIView]) -> UIStackView {
+    let sv = UIStackView(arrangedSubviews: views)
+    sv.axis = .horizontal
+    sv.spacing = 3
+    sv.alignment = .center
+    
+    return sv
+  }
 }
